4f999381e11cafa4bd95382c6d15800e04be3f52
Wadad Mehdi <wadadmehdi@gmail.com>
2023-01-31 17:45:47 +0100

    First Commit

---
+++ AppWS/AppWS/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppWS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SR1.BanqueServiceClient stub = new SR1.BanqueServiceClient();
+            Console.WriteLine(stub.ConversionEuroToDH(80));
+            SR1.compte cp = stub.getCompte(5);
+            Console.WriteLine(cp.code);
+            Console.WriteLine(cp.solde);
+            SR1.compte[] cptes = stub.listComptes();
+            foreach (var c in cptes)
+            {
+                Console.WriteLine("------------------");
+                Console.WriteLine(c.code);
+                Console.WriteLine(c.solde);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
